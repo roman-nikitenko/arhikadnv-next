882884051c71a6b73b83abcc8e07ff2aa2b33e67
roman-nikitenko <roman.nikitenko.js@gmail.com>
2025-03-27 09:28:26 +0200

    Add meta tags and change five icon

--- context/Context.tsx
+++ context/Context.tsx
@@ -1,49 +1,4 @@
 'use client';
-// import React, { FC, createContext, useState } from 'react';
-
-// type initialModalContextType = {
-//   isOpenModal: boolean;
-//   setIsOpenModal: React.Dispatch<React.SetStateAction<boolean>>;
-//   // aboutSectionRef: React.RefObject<HTMLDivElement | null>;
-//   // servicesSectionRef: React.RefObject<HTMLDivElement | null>;
-//   // projectsSectionRef: React.RefObject<HTMLDivElement | null>;
-// }
-
-// const initialModalContext: initialModalContextType = {
-//   isOpenModal: false,
-//   setIsOpenModal: () => {},
-//   // aboutSectionRef: null,
-//   // servicesSectionRef: null,
-//   // projectsSectionRef: null,
-// }
-
-// type ModalContextProps = {
-//   children: React.ReactNode
-// }
-
-// export const ModalContext = createContext<initialModalContextType>(initialModalContext)
-
-// export const ModalContextProvider: FC<ModalContextProps> = ({ children }) => {
-//   const [isOpenModal, setIsOpenModal] = useState<boolean>(false)
-//   // const aboutSectionRef = useRef<HTMLDivElement>(null);
-//   // const servicesSectionRef = useRef<HTMLDivElement>(null);
-//   // const projectsSectionRef = useRef<HTMLDivElement>(null);
-  
-//   const value = {
-//     isOpenModal: isOpenModal,
-//     setIsOpenModal: setIsOpenModal,
-//     // aboutSectionRef: aboutSectionRef,
-//     // servicesSectionRef: servicesSectionRef,
-//     // projectsSectionRef: projectsSectionRef,
-//   }
-  
-//   return (
-//     <ModalContext.Provider value={value}>
-//       {children}
-//     </ModalContext.Provider>
-//   );
-// };
-
 import { createContext, useContext, Dispatch, SetStateAction, useState } from 'react';
 
 type ModalContextType = {
