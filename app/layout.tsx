4f6a3b91ebe80f9599985d9220c6d4ce76e33eef
roman-nikitenko <roman.nikitenko.js@gmail.com>
2025-03-30 20:48:53 +0300

    Add keywords and robots

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,33 +5,43 @@ import { GlobalContextProvider } from "@/context/Context";
 
 const inter = Inter({ subsets: ["latin"] });
 
+const WEB_URL = "https://arhikadnv-next.vercel.app/";
+const WEB_NAME = "Архікад-НВ";
+const WEB_TITLE = "Архікад-НВ - Архітектурні послуги, оформлення документів";
+const WEB_DESCRIPTION = "Архікад-НВ – оформлення документів на будинок під ключ, будівельний паспорт, проєкти на будинки, МАФ, рекламні носії, топозйомка 1:500, геодезія, кадастрові документи.";
+const WEB_IMAGE = WEB_URL + 'arhicadnv.jpg'
+
 export const metadata: Metadata = {
-  title: "Архікад-НВ - Архітектурні послуги, оформлення документів",
-  description:
-    "Архікад-НВ – оформлення документів на будинок під ключ, будівельний паспорт, проєкти на будинки, МАФ, рекламні носії, топозйомка 1:500, геодезія, кадастрові документи.",
-  applicationName: "Архікад-НВ",
-  creator: "Banzaifun",
+  title: WEB_TITLE,
+  description: WEB_DESCRIPTION,
+  applicationName: WEB_NAME,
+  robots: "index, follow",
+  keywords: "архітектурні послуги, оформлення документів, будівельний паспорт, проєкти на будинки, МАФ, рекламні носії, топозйомка, геодезія, кадастрові документи, архітектура",
+
   openGraph: {
-    title: "Архікад-НВ - Архітектурні послуги, оформлення документів",
-    description:
-      "Архікад-НВ – оформлення документів на будинок під ключ, будівельний паспорт, проєкти на будинки, МАФ, рекламні носії, топозйомка 1:500, геодезія, кадастрові документи.",
-    url: "https://arhikadnv.com/",
+    title: WEB_TITLE,
+    description: WEB_DESCRIPTION,
+    url: WEB_URL,
     countryName: "Ukraine",
     images: [
       {
-        url: "https://www.arhikadnv.com/arhicadnv.jpg",
+        url: WEB_IMAGE,
         width: 1200,
         height: 630,
         alt: "Архікад-НВ – архітектурні послуги",
       },
     ],
+    siteName: WEB_NAME,
+    type: "website",
   },
+
+  creator: "Banzaifun",
+
   twitter: {
     card: "summary_large_image",
-    title: "Архікад-НВ - Архітектурні послуги, оформлення документів",
-    description:
-      "Архікад-НВ – оформлення документів на будинок під ключ, будівельний паспорт, проєкти на будинки, МАФ, рекламні носії, топозйомка 1:500, геодезія, кадастрові документи.",
-    images: ["https://www.arhikadnv.com/arhicadnv.jpg"],
+    title: WEB_TITLE,
+    description: WEB_DESCRIPTION,
+    images: WEB_IMAGE,
   },
 };
 
@@ -43,11 +53,10 @@ export default function RootLayout({
   const jsonLd = {
     "@context": "https://schema.org",
     "@type": "Organization",
-    name: "Архікад-НВ",
-    url: "https://www.arhikadnv.com/",
-    logo: "https://www.arhikadnv.com/arhicadnv.jpg",
-    description:
-      "Оформлення документів на будинок під ключ, будівельний паспорт, проєкти на будинки, МАФ, рекламні носії, топозйомка 1:500, геодезія, кадастрові документи.",
+    name: WEB_NAME,
+    url: WEB_URL,
+    logo: WEB_IMAGE,
+    description: WEB_DESCRIPTION ,
     contactPoint: {
       "@type": "ContactPoint",
       telephone: "+380980055404",
