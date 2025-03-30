cb34f52fae96118cbd09cef8bcfede806005a43e
roman-nikitenko <roman.nikitenko.js@gmail.com>
2025-03-30 20:19:38 +0300

    Improve metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,17 +17,21 @@ export const metadata: Metadata = {
       "Архікад-НВ – оформлення документів на будинок під ключ, будівельний паспорт, проєкти на будинки, МАФ, рекламні носії, топозйомка 1:500, геодезія, кадастрові документи.",
     url: "https://arhikadnv.com/",
     countryName: "Ukraine",
-    images: "https://www.arhikadnv.com/arhicadnv.jpg",
-    siteName: "arhikadnv",
-    phoneNumbers: "+380980055404",
-    emails: "arhicadnv@ukr.net",
+    images: [
+      {
+        url: "https://www.arhikadnv.com/arhicadnv.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Архікад-НВ – архітектурні послуги",
+      }
+    ],
   },
   twitter: {
     card: "summary_large_image",
     title: "Архікад-НВ - Архітектурні послуги, оформлення документів",
     description:
       "Архікад-НВ – оформлення документів на будинок під ключ, будівельний паспорт, проєкти на будинки, МАФ, рекламні носії, топозйомка 1:500, геодезія, кадастрові документи.",
-    images: "https://www.arhikadnv.com/arhicadnv.jpg",
+    images: ["https://www.arhikadnv.com/arhicadnv.jpg"],
   },
 };
 
@@ -37,21 +41,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="ua">
-      <meta
-        property="og:title"
-        content="Архікад-НВ - Архітектурні послуги, оформлення документів"
-      />
-      <meta
-        property="og:description"
-        content="Архікад-НВ – оформлення документів на будинок під ключ, будівельний паспорт, проєкти на будинки, МАФ, рекламні носії, топозйомка 1:500, геодезія, кадастрові документи."
-      />
-      <meta
-        property="og:image"
-        content="https://www.arhikadnv.com/arhicadnv.jpg"
-      />
-      <meta property="og:url" content="https://www.arhikadnv.com/" />
-      <meta property="og:type" content="website" />
+    <html lang="uk">
+      
       <body className={inter.className}>
         <GlobalContextProvider>{children}</GlobalContextProvider>
       </body>
